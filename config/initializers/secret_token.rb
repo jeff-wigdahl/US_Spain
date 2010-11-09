5eca1d6fac08c3b88670f1599dc0c7dfe4a0ce24
jwigdahl <jwigdahl@visionquest-bio.com>
2010-11-09 08:56:33 -0700

    HW1 US_Spain

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+USSpain::Application.config.secret_token = 'b45cc5ff0a30a5ff9d0a13facf5338263a37bf9256229b7512f1c1945b028227eaffd0b28687e4951c520f250455b94291486e102c51fe0a4942a1a54320bfb2'
